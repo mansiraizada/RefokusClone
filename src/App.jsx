21c6025229ca39ebe095265fa0a23d13e52b22f4
mansiraizada <mansi23raizada@gmail.com>
2024-02-06 21:16:02 +0530

    Work component started

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Work from './components/Work'
 
 
 const App = () => {
   return (
     <div className='w-full h-screen font-["Satoshi Variable"] text-white bg-zinc-900'>
       <Navbar/>
+      <Work/>
     </div>
   )
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div className='max-w-screen-xl mx-auto bg-sky-300'>Work</div>
+  )
+}
+
+export default Work
