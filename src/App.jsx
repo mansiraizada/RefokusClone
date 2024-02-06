f057c4ab0e4391cdddcfb99528769f73ebe3197a
mansiraizada <mansi23raizada@gmail.com>
2024-02-06 21:59:50 +0530

    Stripes started

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Work from './components/Work'
+import Stripes from './components/Stripes'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className='w-full h-screen font-["Satoshi Variable"] text-white bg-zinc-900'>
       <Navbar/>
       <Work/>
+      <Stripes/>
     </div>
   )
 }
